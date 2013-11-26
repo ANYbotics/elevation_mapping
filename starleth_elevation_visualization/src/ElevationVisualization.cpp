f8071b62b9ffe4aff5edc69e174714a9e1198726
Peter Fankhauser <pfankhauser@ethz.ch>
2013-11-26 12:13:37 +0100

    Initial commmit for starleth_evevation_visualization

---
+++ starleth_elevation_visualization/src/ElevationVisualization.cpp
@@ -0,0 +1,23 @@
+/*
+ * ElevationVisualization.cpp
+ *
+ *  Created on: Nov 19, 2013
+ *      Author: Péter Fankhauser
+ *	 Institute: ETH Zurich, Autonomous Systems Lab
+ */
+#include "ElevationVisualization.hpp"
+
+namespace starleth {
+
+ElevationVisualization::ElevationVisualization(ros::NodeHandle& nodeHandle)
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+ElevationVisualization::~ElevationVisualization()
+{
+  // TODO Auto-generated destructor stub
+}
+
+} /* namespace footholdfinder */
---
+++ starleth_elevation_visualization/src/starleth_elevation_visualization_node.cpp
@@ -0,0 +1,25 @@
+/*
+ * starleth_elevation_visualization_node.cpp
+ *
+ *  Created on: Oct 3, 2013
+ *      Author: Péter Fankhauser
+ *   Institute: ETH Zurich, Autonomous Systems Lab
+ */
+
+#include <ros/ros.h>
+#include "ElevationVisualization.hpp"
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "starleth_elevation_visualization");
+
+  // Logger
+  log4cxx::LoggerPtr logger = log4cxx::Logger::getLogger(ROSCONSOLE_DEFAULT_NAME);
+  logger->setLevel(ros::console::g_level_lookup[ros::console::levels::Debug]);
+
+  ros::NodeHandle nodeHandle;
+
+  starleth::ElevationVisualization elevationVisualization(nodeHandle);
+
+  return 0;
+}
