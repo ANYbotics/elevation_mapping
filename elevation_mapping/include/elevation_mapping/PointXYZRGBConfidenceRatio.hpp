76ee69c0d5968695413252d1b7a5ad96d7c3429f
pg95cm <pgomez@robotnik.es>
2021-09-17 13:21:42 +0200

    endif

--- elevation_mapping/include/elevation_mapping/PointXYZRGBConfidenceRatio.hpp
+++ elevation_mapping/include/elevation_mapping/PointXYZRGBConfidenceRatio.hpp
@@ -19,7 +19,7 @@ namespace pcl {
 
 #ifndef PCL_MAKE_ALIGNED_OPERATOR_NEW
 #define PCL_MAKE_ALIGNED_OPERATOR_NEW EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-
+#endif
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wpedantic"
 struct _PointXYZRGBConfidenceRatio {
