412706fcd499383d13891c9cd2a7839efcac7a66
Maximilian Wulf <mwulf@anybotics.com>
2021-12-09 08:37:50 +0000

    Merge branch 'feature/elevation_mapping/fix_clang_tidy_warnings_5' into 'master'
    [elevation_mapping] NOLINTING warnings in PointXYZRGBConfidenceRatio.hpp
    
    GitOrigin-RevId: 1ec2f7ec88336c58496c0158544b3cfe69dc1b5b

--- elevation_mapping/include/elevation_mapping/PointXYZRGBConfidenceRatio.hpp
+++ elevation_mapping/include/elevation_mapping/PointXYZRGBConfidenceRatio.hpp
@@ -19,36 +19,36 @@ namespace pcl {
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wpedantic"
-struct _PointXYZRGBConfidenceRatio {
+struct _PointXYZRGBConfidenceRatio {  // NOLINT(cppcoreguidelines-pro-type-union-access)
   PCL_ADD_POINT4D;  // NOLINT(cppcoreguidelines-pro-type-union-access, readability-const-return-type, modernize-avoid-c-arrays) This adds
                     // the members x,y,z which can also be accessed using the point (which is float[4])
   PCL_ADD_RGB;      // NOLINT(cppcoreguidelines-pro-type-union-access, readability-const-return-type)
   union {
     struct {
-      float confidence_ratio;
+      float confidence_ratio;  // NOLINT(readability-identifier-naming)
     };
-    float data_c[4];
+    float data_c[4];  // NOLINT(readability-identifier-naming, modernize-avoid-c-arrays)
   };
   PCL_MAKE_ALIGNED_OPERATOR_NEW
 } EIGEN_ALIGN16;  // enforce SSE padding for correct memory alignment
 #pragma GCC diagnostic pop
 
 struct PointXYZRGBConfidenceRatio : public _PointXYZRGBConfidenceRatio {
-  inline PointXYZRGBConfidenceRatio(const _PointXYZRGBConfidenceRatio& p) {
+  inline explicit PointXYZRGBConfidenceRatio(const _PointXYZRGBConfidenceRatio& p) : _PointXYZRGBConfidenceRatio() {
     // XZY
-    x = p.x;
-    y = p.y;
-    z = p.z;
-    data[3] = 1.0f;
+    x = p.x;         // NOLINT(cppcoreguidelines-pro-type-union-access)
+    y = p.y;         // NOLINT(cppcoreguidelines-pro-type-union-access)
+    z = p.z;         // NOLINT(cppcoreguidelines-pro-type-union-access)
+    data[3] = 1.0f;  // NOLINT(cppcoreguidelines-pro-type-union-access)
 
     // RGB
-    rgba = p.rgba;
+    rgba = p.rgba;  // NOLINT(cppcoreguidelines-pro-type-union-access)
 
     // Confidence
-    confidence_ratio = p.confidence_ratio;
+    confidence_ratio = p.confidence_ratio;  // NOLINT(cppcoreguidelines-pro-type-union-access)
   }
 
-  inline PointXYZRGBConfidenceRatio(float _confidence_ratio = 1.f)
+  inline explicit PointXYZRGBConfidenceRatio(float _confidence_ratio = 1.f)
       : PointXYZRGBConfidenceRatio(0.f, 0.f, 0.f, 0, 0, 0, _confidence_ratio) {}
 
   inline PointXYZRGBConfidenceRatio(std::uint8_t _r, std::uint8_t _g, std::uint8_t _b)
@@ -57,18 +57,19 @@ struct PointXYZRGBConfidenceRatio : public _PointXYZRGBConfidenceRatio {
   inline PointXYZRGBConfidenceRatio(float _x, float _y, float _z) : PointXYZRGBConfidenceRatio(_x, _y, _z, 0, 0, 0) {}
 
   inline PointXYZRGBConfidenceRatio(float _x, float _y, float _z, std::uint8_t _r, std::uint8_t _g, std::uint8_t _b,
-                                    float _confidence_ratio = 1.f) {
-    x = _x;
-    y = _y;
-    z = _z;
-    data[3] = 1.0f;
+                                    float _confidence_ratio = 1.f)
+      : _PointXYZRGBConfidenceRatio() {
+    x = _x;          // NOLINT(cppcoreguidelines-pro-type-union-access)
+    y = _y;          // NOLINT(cppcoreguidelines-pro-type-union-access)
+    z = _z;          // NOLINT(cppcoreguidelines-pro-type-union-access)
+    data[3] = 1.0f;  // NOLINT(cppcoreguidelines-pro-type-union-access)
 
-    r = _r;
-    g = _g;
-    b = _b;
-    a = 255;
+    r = _r;   // NOLINT(cppcoreguidelines-pro-type-union-access)
+    g = _g;   // NOLINT(cppcoreguidelines-pro-type-union-access)
+    b = _b;   // NOLINT(cppcoreguidelines-pro-type-union-access)
+    a = 255;  // NOLINT(cppcoreguidelines-pro-type-union-access)
 
-    confidence_ratio = _confidence_ratio;
+    confidence_ratio = _confidence_ratio;  // NOLINT(cppcoreguidelines-pro-type-union-access)
   }
 
   friend std::ostream& operator<<(std::ostream& os, const PointXYZRGBConfidenceRatio& p);
@@ -82,11 +83,12 @@ namespace elevation_mapping {
 using PointCloudType = pcl::PointCloud<pcl::PointXYZRGBConfidenceRatio>;
 }  // namespace elevation_mapping
 
-POINT_CLOUD_REGISTER_POINT_STRUCT(pcl::_PointXYZRGBConfidenceRatio,             // here we assume a XYZ + "confidence_ratio" (as fields)
-                                  (float, x, x)                                 // NOLINT
-                                  (float, y, y)                                 // NOLINT
-                                  (float, z, z)                                 // NOLINT
-                                  (std::uint32_t, rgba, rgba)                   // NOLINT
+POINT_CLOUD_REGISTER_POINT_STRUCT(pcl::_PointXYZRGBConfidenceRatio,  // NOLINT(modernize-avoid-c-arrays, readability-const-return-type) here
+                                                                     // we assume a XYZ + "confidence_ratio" (as fields)
+                                  (float, x, x)                      // NOLINT
+                                  (float, y, y)                      // NOLINT
+                                  (float, z, z)                      // NOLINT
+                                  (std::uint32_t, rgba, rgba)        // NOLINT
                                   (float, confidence_ratio, confidence_ratio))  // NOLINT
 
 POINT_CLOUD_REGISTER_POINT_WRAPPER(pcl::PointXYZRGBConfidenceRatio, pcl::_PointXYZRGBConfidenceRatio)
